d2914b2c2b1400072e1738e33dc647e81d242100
tomaszbunio <50107959+tomaszbunio@users.noreply.github.com>
2026-07-27 17:23:40 +0200

    Add files via upload

--- convert_logos.bat
+++ convert_logos.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d "%~dp0"
 if exist "%USERPROFILE%\.platformio\penv\Scripts\python.exe" (
-  "%USERPROFILE%\.platformio\penv\Scripts\python.exe" scripts\convert_logos.py --all-resolutions
+  "%USERPROFILE%\.platformio\penv\Scripts\python.exe" scripts\convert_logos.py --all-resolutions --copy-to-www
 ) else (
-  python scripts\convert_logos.py --all-resolutions
+  python scripts\convert_logos.py --all-resolutions --copy-to-www
 )
 pause
